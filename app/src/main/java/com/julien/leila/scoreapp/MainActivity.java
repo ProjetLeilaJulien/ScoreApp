d688c49060e35dcafd7ee9263e89da63f15d4d26
JulienRouse <julien.rouse@gmail.com>
2015-07-29 23:12:00 +0200

    demarrage du projet

---
+++ app/src/main/java/com/julien/leila/scoreapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.julien.leila.scoreapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(text);
+    }
+}
